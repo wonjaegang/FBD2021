aae06104c9951e15103d6dca88e66c4c11c146c6
Wonjaegang <robinsam@naver.com>
2021-01-10 16:42:52 +0900

    update random_algorithm.py

--- random_algorithm.py
+++ random_algorithm.py
@@ -1,6 +1,7 @@
 import random
+import game_settings
 
 
-def select_counting(previous_num):
-    counting = random.choice(list(range(1, 4)))
+def select_counting(_):
+    counting = random.choice(list(range(1, 1 + game_settings.max_count)))
     return counting
