e56ef35fcdfab31def79a6f303448814ffd08048
TM.kmkim <kmkim@toysmyth.com>
2021-01-11 20:49:44 +0900

    rename module

--- Kyungho_algorithm.py
+++ Kyungho_algorithm.py
@@ -2,9 +2,9 @@ import random
 
 def select_counting(previous_num):
         previous_num += 1
-        count=0;
-        num_count=random.randragne(0,3)
-        for i in num_count:
+        count=0
+        num_count=random.randrange(0,3)
+        for i in range(1, num_count):
             count+=1
             if previous_num == 28 or previous_num == 29 or previous_num == 22 or previous_num == 15 or previous_num == 8:
                 if count!=3:
