0860de11e070a1b18fce11e1f4f160d9d2f5b456
mia <58319332+rudgh1236@users.noreply.github.com>
2021-01-13 13:44:08 +0900

    Update Kyungho_algorithm.py

--- Kyungho_algorithm.py
+++ Kyungho_algorithm.py
@@ -5,13 +5,20 @@ def select_counting(previous_num):
         mynum=previous_num
         num_count=random.randrange(1,4)
         good=[8,15,22,28,29]
+        good2=[7,14,21,27,28]
         bad=[10,18,25,30]
         for i in range(num_count):
             count+=1
             mynum=mynum+1
+            if mynum in good2:
+                if count == 1:
+                        return 3
             if mynum in good:
-                if count!=3 and num_count!=3: 
-                    return num_count+1     
+                if count==1:
+                        return 2
+                elif count==2:
+                        return 3
+              
             if mynum in bad:
                 if count==1:
                         return 1
