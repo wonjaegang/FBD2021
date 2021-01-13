99cf1d1ff156bfe47a426b0d6089ae67fcb72cc7
TM.kmkim <kmkim@toysmyth.com>
2021-01-13 14:35:32 +0900

    Organizing

--- Kyeongmin_algorithm.py
+++ Kyeongmin_algorithm.py
@@ -1,19 +1,17 @@
 import random
 
 def select_counting(previous_num):
-        previous_num += 1
-        mod = previous_num % 7
-        if mod == 0:
-            if previous_num == 28:
-                return random.randrange(2, 4)
-            else:
-                return 3
-        elif mod == 1:
-            if previous_num == 29:
-                return random.randrange(1, 3)
-            else:
-                return 2
-        elif mod == 2 or mod == 3:
-            return 1
+    previous_num += 1
+    mod = previous_num % 7
+    if mod == 2 or mod == 3:
+        return 1
+    elif mod == 1:
+        if previous_num == 29:
+            return random.randrange(1, 3)
+        else:
+            return 2
+    else:
+        if previous_num == 28:
+            return random.randrange(2, 4)
         else:
             return 3
