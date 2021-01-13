8764095ebdd77a2d0fc211a83b394682295ae082
Wonjaegang <robinsam@naver.com>
2021-01-13 15:19:17 +0900

    Add flush_previous_data func
    - There was a fatal bugs : the data sets(opponent_counting_odds, memoization) were not initialized before select_counting function. From now on, flush_previous_data function will support it.

--- Jaewon_algorithm.py
+++ Jaewon_algorithm.py
@@ -5,11 +5,15 @@ import random
 
 magic_num = setting.magic_num
 max_counting = setting.max_counting
+my_counting_range = range(max_counting)
 number_of_opponents = setting.number_of_players - 1
 opponents_counting_min = number_of_opponents * 1
 opponents_counting_max = number_of_opponents * max_counting
 opponents_counting_range = range(opponents_counting_min, opponents_counting_max + 1)
+
 opponents_counting_data = []
+game_data = []
+memoization = {}
 
 data_sheet = "%s_data_sheet.txt" % "Jaewon_algorithm"
 
@@ -29,9 +33,6 @@ def load_data_sheet():
             opponents_counting_data.append(split_int_list)
 
 
-memoization = {}
-
-
 def calculate_win_rate(previous_num, my_counting):
     my_last_num = previous_num + my_counting
 
@@ -45,7 +46,6 @@ def calculate_win_rate(previous_num, my_counting):
     win_rate = 0
     for i, opponents_counting in enumerate(opponents_counting_range):
         next_previous_num = my_last_num + opponents_counting
-        my_counting_range = range(max_counting)
         win_rate_by_counting = list(map(lambda x: calculate_win_rate(next_previous_num, x + 1), my_counting_range))
         odds = opponents_counting_data[my_last_num - 1][i] / sum(opponents_counting_data[my_last_num - 1])
         win_rate = win_rate + max(win_rate_by_counting) * odds
@@ -55,7 +55,6 @@ def calculate_win_rate(previous_num, my_counting):
 
 
 def adjust_data_sheet(my_numbers):
-    load_data_sheet()
     for i in range(len(my_numbers) - 1):
         opponents_counting = my_numbers[i + 1][0] - my_numbers[i][1]
         temp = opponents_counting_data[my_numbers[i][1] - 1][opponents_counting - opponents_counting_min]
@@ -74,9 +73,17 @@ def random_max_index(a):
     return random.choice(max_index_list)
 
 
+def flush_previous_data():
+    opponents_counting_data.clear()
+    game_data.clear()
+    memoization.clear()
+
+
 def select_counting(previous_num):
+    flush_previous_data()
     load_data_sheet()
-    counting_range = range(max_counting)
-    win_rate_by_counting = list(map(lambda x: calculate_win_rate(previous_num, x + 1), counting_range))
+    win_rate_by_counting = list(map(lambda x: calculate_win_rate(previous_num, x + 1), my_counting_range))
+    print(win_rate_by_counting)
+    print(memoization)
     counting = random_max_index(win_rate_by_counting) + 1
     return counting
