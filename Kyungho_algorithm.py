ef20f9031bb2c025383bc583e2f33116763f713a
mia <58319332+rudgh1236@users.noreply.github.com>
2021-01-13 13:07:52 +0900

    Update Kyungho_algorithm.py

--- Kyungho_algorithm.py
+++ Kyungho_algorithm.py
@@ -2,10 +2,11 @@ import random
 
 def select_counting(previous_num):
         count=0
+        mynum=previous_num
         num_count=random.randrange(0,3)
         for i in range(1, num_count):
             count+=1
-            mynum=previous_num+1
+            mynum=mynum+1
             if mynum == 28 or mynum == 29 or mynum == 22 or mynum == 15 or mynum == 8:
                 if count!=3:
                     return num_count+1
