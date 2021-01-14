645db760b1260e0df7835587be76f53269769bc0
TM.kmkim <kmkim@toysmyth.com>
2021-01-14 09:46:04 +0900

    code diet

--- Kyeongmin_algorithm.py
+++ Kyeongmin_algorithm.py
@@ -1,17 +1,16 @@
 import random
 
 def select_counting(previous_num):
-    previous_num += 1
-    mod = previous_num % 7
+    mod = (previous_num + 1) % 7
     if mod == 2 or mod == 3:
         return 1
     elif mod == 1:
-        if previous_num == 29:
+        if previous_num == 28:
             return random.randrange(1, 3)
         else:
             return 2
     else:
-        if previous_num == 28:
+        if previous_num == 27:
             return random.randrange(2, 4)
         else:
             return 3
